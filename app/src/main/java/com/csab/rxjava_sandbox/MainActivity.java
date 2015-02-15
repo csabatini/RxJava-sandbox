2f1fa4ca816045cdc756bf360ac69cea2a61ef95
Caleb Sabatini <calebsabatini@gmail.com>
2015-02-14 20:18:13 -0600

    initial commit

---
+++ app/src/main/java/com/csab/rxjava_sandbox/MainActivity.java
@@ -0,0 +1,35 @@
+package com.csab.rxjava_sandbox;
+
+import android.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, new Fragment(), "frag")
+                .commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/test/java/com/csab/rxjava_sandbox/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.csab.rxjava_sandbox;
+
+import android.app.Fragment;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowSupportMenuInflater;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.ANDROID.assertThat;
+
+@Config(emulateSdk = 18, reportSdk = 18, shadows = {ShadowSupportMenuInflater.class})
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    private MainActivity mActivity;
+
+    @Before
+    public void setup() {
+        mActivity = Robolectric.buildActivity(MainActivity.class).create().resume().visible().get();
+    }
+
+    @Test
+    public void testActivityState() {
+        assertThat(mActivity).isNotNull().isNotFinishing();
+    }
+
+    @Test
+    public void testFragmentState() {
+        Fragment fragment = mActivity.getFragmentManager().findFragmentByTag("frag");
+        assertThat(fragment).isNotNull().isAdded().isResumed();
+    }
+
+}
